8fdc1ecf4cec5beccab9b37f91d13594574222b7
Krzysztof Nadlonek <Krzysztof.Nadlonek@external.bcgdv.com>
2024-02-14 15:21:02 +0100

    feat(er-20240214): adds shadcn and clsx tools

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,26 +1,27 @@
 'use client';
  
 import { useChat } from 'ai/react';
+import { clsx } from 'clsx';
  
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <section className="h-screen flex flex-col w-full max-w-2xl mx-auto my-8 stretch rounded shadow-xl bg-slate-50">
       {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
+        <div key={m.id} className={clsx('whitespace-pre-wrap p-2 shadow-xl', m.role === 'user' ? 'bg-slate-100' : 'bg-slate-200')}>
+          <span className='font-bold'>{m.role === 'user' ? 'User: ' : 'AI: '}</span>
           {m.content}
         </div>
       ))}
  
       <form onSubmit={handleSubmit}>
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          className="fixed bottom-0 w-full max-w-2xl p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
         />
       </form>
-    </div>
+    </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-row items-center justify-center gap-6">
-      <div className="flex flex-col items-center gap-1">
+    <main className="flex h-screen flex-col items-center justify-center gap-6">
+      <div className="flex flex-col items-center">
         <span className="text-4xl font-extrabold tracking-normal lg:text-6xl">
           Note Chat App
         </span>
         <p className="pt-2">Your personal note assistant!</p>
+      </div>
+      <div className="flex flex-row gap-2">
+      <Button asChild>
+        <Link href="/chat">Chat</Link>
+      </Button>
+      <Button variant='outline' asChild>
+        <Link href="/posts">Posts</Link>
+      </Button>
       </div>
     </main>
   );
