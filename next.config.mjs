b1d503cc8eb0f43ce2f62084191bdbee827455fb
Krzysztof Nadlonek <Krzysztof.Nadlonek@external.bcgdv.com>
2024-02-19 08:14:10 +0100

    feat(er-20240219): adds web vitals, bundle analizer and custom fonts configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// Run the following command to analyze your bundles:
+// ANALYZE=true pnpm build
+//The report will open three new tabs in your browser, which you can inspect
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
-export default nextConfig;
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  //To show fetches logs with full urls
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
+
+export default bundleAnalyzer(nextConfig);
