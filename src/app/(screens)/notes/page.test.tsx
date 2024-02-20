5ee2e36c3f311b75302de75d50b33dd86b8aed24
Krzysztof Nadlonek <Krzysztof.Nadlonek@external.bcgdv.com>
2024-02-20 09:48:10 +0100

    feat(er-20240220): renamed component into notes

--- src/app/(screens)/notes/page.test.tsx
+++ src/app/(screens)/notes/page.test.tsx
@@ -5,9 +5,9 @@ global.fetch = jest.fn(() =>
   Promise.resolve({ ok: true, json: () => Promise.resolve([]) })
 ) as jest.Mock;
 
-describe("Posts", () => {
+describe("Notes", () => {
   it("shows page title", async () => {
     render(await Page());
-    expect(screen.getByRole("heading")).toHaveTextContent("Your recent posts:");
+    expect(screen.getByRole("heading")).toHaveTextContent("Your recent notes:");
   });
 })
