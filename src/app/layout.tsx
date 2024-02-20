e74a59e7720124b388143ec153b7ca39b9645d10
Krzysztof Nadlonek <Krzysztof.Nadlonek@external.bcgdv.com>
2024-02-20 14:09:33 +0100

    feat(er-20240219): adds comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,8 @@ import { WebVitals } from "./web-vitals";
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
-  display: 'swap',
+  // Display (font-display) defines how the browser behaves during download
+  display: 'swap', // Instructs the browser to use the fallback font to display the text until the custom font has fully downloaded
 })
 
 const roboto_mono = Roboto_Mono({
